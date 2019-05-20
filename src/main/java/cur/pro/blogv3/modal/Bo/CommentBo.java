6489b0ae907b8697d0aa6a9892069083842041a8
yezijian <1677390657@qq.com>
2019-05-20 09:12:00 +0800

    do some change

---
+++ src/main/java/cur/pro/blogv3/modal/Bo/CommentBo.java
@@ -0,0 +1,42 @@
+package cur.pro.blogv3.modal.Bo;
+
+import cur.pro.blogv3.modal.Vo.CommentVo;
+
+import java.util.List;
+
+
+public class CommentBo extends CommentVo {
+
+    private int levels;
+    private List<CommentVo> children;
+
+    public CommentBo(CommentVo comments) {
+        setAuthor(comments.getAuthor());
+        setMail(comments.getMail());
+        setCoid(comments.getCoid());
+        setAuthorId(comments.getAuthorId());
+        setUrl(comments.getUrl());
+        setCreated(comments.getCreated());
+        setAgent(comments.getAgent());
+        setIp(comments.getIp());
+        setContent(comments.getContent());
+        setOwnerId(comments.getOwnerId());
+        setCid(comments.getCid());
+    }
+
+    public int getLevels() {
+        return levels;
+    }
+
+    public void setLevels(int levels) {
+        this.levels = levels;
+    }
+
+    public List<CommentVo> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<CommentVo> children) {
+        this.children = children;
+    }
+}
