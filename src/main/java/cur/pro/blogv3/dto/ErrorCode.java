d1f029b5cf65d553a6af1d6cbc9e5b40d8b79be1
yezijian <1677390657@qq.com>
2019-05-20 09:45:58 +0800

    add dto

---
+++ src/main/java/cur/pro/blogv3/dto/ErrorCode.java
@@ -0,0 +1,13 @@
+package cur.pro.blogv3.dto;
+
+/**
+ * 错误提示
+ */
+public interface ErrorCode {
+
+    /**
+     * 非法请求
+     */
+    String BAD_REQUEST = "BAD REQUEST";
+
+}
---
+++ src/main/java/cur/pro/blogv3/dto/MetaDto.java
@@ -0,0 +1,16 @@
+package cur.pro.blogv3.dto;
+
+import cur.pro.blogv3.modal.Vo.MetaVo;
+
+public class MetaDto extends MetaVo {
+
+    private int count;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
